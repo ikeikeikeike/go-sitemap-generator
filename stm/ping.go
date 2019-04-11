161ff407f26473d850e110fab4444d2ea541b2f9
Zheng-Xiang Ke <kf99916@gmail.com>
2019-04-11 17:55:44 +0800

    Ping the alternative urls

--- stm/ping.go
+++ stm/ping.go
@@ -12,27 +12,34 @@ func PingSearchEngines(opts *Options, urls ...string) {
 		"http://www.google.com/webmasters/tools/ping?sitemap=%s",
 		"http://www.bing.com/webmaster/ping.aspx?siteMap=%s",
 	}...)
-	sitemapURL := opts.IndexLocation().URL()
+
+	indexLocation := opts.IndexLocation()
+	sitemapURLs := []string{indexLocation.URL()}
+	for _, alternativeURL := range indexLocation.AlternativeURLs() {
+		sitemapURLs = append(sitemapURLs, alternativeURL)
+	}
 
 	bufs := len(urls)
 	does := make(chan string, bufs)
 	client := http.Client{Timeout: time.Duration(5 * time.Second)}
 
 	for _, url := range urls {
-		go func(baseurl string) {
-			url := fmt.Sprintf(baseurl, sitemapURL)
-			println("Ping now:", url)
+		for _, sitemapURL := range sitemapURLs {
+			go func(baseurl string) {
+				url := fmt.Sprintf(baseurl, sitemapURL)
+				println("Ping now:", url)
 
-			resp, err := client.Get(url)
-			if err != nil {
-				does <- fmt.Sprintf("[E] Ping failed: %s (URL:%s)",
-					err, url)
-				return
-			}
-			defer resp.Body.Close()
+				resp, err := client.Get(url)
+				if err != nil {
+					does <- fmt.Sprintf("[E] Ping failed: %s (URL:%s)",
+						err, url)
+					return
+				}
+				defer resp.Body.Close()
 
-			does <- fmt.Sprintf("Successful ping of `%s`", url)
-		}(url)
+				does <- fmt.Sprintf("Successful ping of `%s`", url)
+			}(url)
+		}
 	}
 
 	for i := 0; i < bufs; i++ {
