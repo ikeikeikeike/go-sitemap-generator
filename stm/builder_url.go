9b8fd004e96cd0476198d634c93c3cd48d20e419
sawamura-takahiro <sawamura-takahiro@dmm.com>
2021-09-01 23:21:38 +0900

    Fix: add option of timeFormat on lastmod

--- stm/builder_url.go
+++ stm/builder_url.go
@@ -99,7 +99,10 @@ func (su *sitemapURL) XML() []byte {
 	SetBuilderElementValue(url, su.data.URLJoinBy("loc", "host", "loc"), "loc")
 	if _, ok := SetBuilderElementValue(url, su.data, "lastmod"); !ok {
 		lastmod := url.CreateElement("lastmod")
-		lastmod.SetText(time.Now().Format("2006-01-02"))
+		lastmod.SetText(time.Now().Format(time.RFC3339))
+		if su.opts.dateFormatYYYYMMDD {
+			lastmod.SetText(time.Now().Format("2006-01-02"))
+		}
 	}
 	if _, ok := SetBuilderElementValue(url, su.data, "changefreq"); !ok {
 		changefreq := url.CreateElement("changefreq")
