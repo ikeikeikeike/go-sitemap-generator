c9a084e020863239d0ea59e1670dd2b0308e7965
ali <sralitorabi@gmail.com>
2023-07-05 16:31:18 +0330

    remove sitemap unnecessary headers

--- stm/consts.go
+++ stm/consts.go
@@ -63,9 +63,6 @@ func NewXmlHeaderGenerator() *HeaderGenerator {
 	return &HeaderGenerator{
 		xml: `<?xml version="1.0" encoding="UTF-8"?>
       <urlset
-        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-          http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
         xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" `,
 	}
 }
